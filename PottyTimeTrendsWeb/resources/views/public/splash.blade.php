384ec9a5f0f22946e6a787acb925d41247661bd3
Philip Wrinkle <philip.wrinkle@gmail.com>
2018-10-01 00:47:17 -0400

    Initial commit of most of the routes, but public facing pages are workin

---
+++ PottyTimeTrendsWeb/resources/views/public/splash.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Welcome!</h1>
+            <p>Name pending. Placeholder about all the cool stuff!</p>
+            <p>Catchy things that we do differently!</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h3>Reason Title</h3>
+            <p>A reason you should try this!</p>
+        </div>
+        <div class="col-md-4">
+            <h3>Reason Title</h3>
+            <p>A reason you should try this!</p>
+        </div>
+        <div class="col-md-4">
+            <h3>Reason Title</h3>
+            <p>A reason you should try this!</p>
+        </div>
+    </div>
+
+@endsection
